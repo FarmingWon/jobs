1d869289e297ec08c644b18d60f9c9fa3342e99b
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-20 17:40:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -399,7 +399,7 @@ def view():
             else:
                 st.session_state.companys = gangso_df
 
-            if st.session_state.show_more == None or len(st.session_state.show_more) != len(st.session_state.companys) or 'show_more' not in st.session_state:
+            if 'show_more' not in st.session_state or st.session_state.show_more == None or len(st.session_state.show_more) != len(st.session_state.companys):
                 st.session_state.show_more = dict.fromkeys([i for i in range(len(st.session_state.companys))], False)
             show_more = st.session_state.show_more
             
