e5f56004e49745a1d75a81dcc4ebf8e4a89a1dac
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-20 12:42:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -379,7 +379,6 @@ def recom():
                         st.session_state_companys = company_df
                     else:
                         st.session_state_companys = gangso_df
-                    st.session_state_companys['선택'] = st.button('선택')
                     st.table(st.session_state_companys)
 
 def map():
