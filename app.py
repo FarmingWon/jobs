669bd45dd0b783f166bd521f276e70fce590e124
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-20 16:43:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -413,13 +413,13 @@ def view():
                 col1, col2 = st.columns(2)
                 col1.write(row['기업명'])
                 placeholder = col2.empty()
-                st.write(show_more)
                 if show_more[int(idx)]:
                     placeholder.button(
                         "less", key=str(idx) + "_", on_click=on_less_click, args=[show_more, idx]
                     )
-                    
+                    print("make_score")
                     make_score(row['기업명'], row['기업위치'], row['기업규모'])
+                    print("end---")
                     st.write('기업규모 : ' + row['기업규모'])
                     st.write('근로계약 : ' + row['근로계약'])
                     st.write('근무시간 : ' + row['근무시간'])
