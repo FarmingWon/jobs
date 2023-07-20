d9f7ab6e500b82bc9793cf2ff56094885897322d
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-20 16:41:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -413,6 +413,7 @@ def view():
                 col1, col2 = st.columns(2)
                 col1.write(row['기업명'])
                 placeholder = col2.empty()
+                st.write(show_more)
                 if show_more[int(idx)]:
                     placeholder.button(
                         "less", key=str(idx) + "_", on_click=on_less_click, args=[show_more, idx]
