455253a33898f458f4a3533f1d01901d855acc27
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-21 12:06:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ def set_variable():
 
 def main():
     html = """
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <div class="title"><h3>🎈Balance UP!</h3></div>
     <div><p>반가워요. BalanceUP 직업추천 서비스에요.</p><div>
     <hr/>
@@ -24,12 +27,28 @@ def main():
         <div class="About_title">
             <h4><span style="color:#DC2D1C">BalanceUP</span>에 대해...</h4>
         </div>
-        <div class="subtitle">
-            <h6><span>BalanceUP은 무엇인가요?</span></h6>
+        <div class="accordition accordition-flush" id="AboutOne">
+            <div class="accordition-item">
+                <h5 class="accordition-header" id="flush-headingOne">
+                    <button
+                        class="accordion-button collapsed"
+                        type="button"
+                        data-mdb-toggle="collapse"
+                        data-mdb-target="#flush-collapseOne"
+                        aria-expanded="false"
+                        aria-controls="flush-collapseOne"
+                      >
+                        BalanceUP은 어떤 서비스일까?
+                    </button>
+                </h5>
+                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-mdb-parent="#AboutOne">
+                    <div class="accordition-body">
+                        <p>내용</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="context">
-            <p><span>BalanceUP이 하는 일</span></p>
-        </div>
+        
     </div>
     """
     st.markdown(html, unsafe_allow_html=True)
