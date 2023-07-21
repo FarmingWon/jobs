3c7652f95d1631481eb856b63d17883443b5ee76
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-21 15:10:01 +0900

    Update 3_🌏_직장_주변_인프라_확인.py

--- pages/3_🌏_직장_주변_인프라_확인.py
+++ pages/3_🌏_직장_주변_인프라_확인.py
@@ -226,7 +226,7 @@ def makeMarker(m, df, color, icon):
                  ).add_to(m)
 
 st.title('🌏 직장 주변 인프라 확인')
-if 'score' not in st.session_score:
+if 'score' not in st.session_state:
     st.error('⚠ 아직 직장 선택이 되지 않았어요! 이전 단계를 먼저 진행해주세요!')
 with st.sidebar:
     htmlSide=f"""
