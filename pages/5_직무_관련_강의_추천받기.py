7aa99153904a5a1ffb8a07c608fe2ca04a191885
wonseok <98411696+FarmingWon@users.noreply.github.com>
2023-07-22 17:45:49 +0900

    update

---
+++ pages/5_직무_관련_강의_추천받기.py
@@ -0,0 +1,43 @@
+import base64
+from pathlib import Path
+import streamlit as st
+
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+
+
+html = """
+<h1>준비 중 입니다.</h1>
+"""
+
+st.markdown(html, unsafe_allow_html=True)
+with st.sidebar:
+    htmlSide=f"""
+        <br/>
+        <p>text.</p>
+    """
+    st.markdown(htmlSide, unsafe_allow_html=True)
+    st.sidebar.markdown("---")
+    htmlSide2=f"""
+        <div id="logo">
+            <h5>
+                <span>Powered By  &nbsp; &nbsp; &nbsp;</span>
+                <img src="data:image/png;base64,{img_to_bytes("./img/openai_logo-removebg.PNG")}" style="width:180px; height:60px;">
+            </h5>
+        </div>
+        <div id="logo">
+            <h5>
+                <span>Powered By  &nbsp; &nbsp; &nbsp;</span>
+                <img src="data:image/png;base64,{img_to_bytes("./img/mongodb logo.PNG")}" style="width:180px; height:60px;">
+            </h5>
+        </div>
+        <div id="logo">
+            <h5>
+                <span>Powered By  &nbsp; &nbsp; &nbsp;</span>
+                <img src="data:image/png;base64,{img_to_bytes("./img/Neo4j-logo_color.PNG")}" style="width:180px; height:60px;">
+            </h5>
+        </div>
+        """
+    st.markdown(htmlSide2, unsafe_allow_html=True)
