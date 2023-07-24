6c3c6c2a0bf499540e5af86b532e04c4d0009c80
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-24 20:51:00 +0900

    Update 밸런스_업.py

--- 밸런스_업.py
+++ 밸런스_업.py
@@ -214,7 +214,7 @@ def main():
         background-color: #0C377A;
         color: #ffffff;
         width: 150%;
-        height: 105%;
+        height: 120%;
     }
     </style>""", unsafe_allow_html=True)
     col1, col2, col3 = st.columns(3)
