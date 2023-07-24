671baf3017bea37367c6787d3144fd3564a9f39f
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-24 15:06:09 +0900

    Update 1_📝_이력서를_통한_직업_추천.py

--- pages/1_📝_이력서를_통한_직업_추천.py
+++ pages/1_📝_이력서를_통한_직업_추천.py
@@ -18,7 +18,7 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import base64
 from pathlib import Path
 
-from 2_👜_직장 선택 import choose
+from pages.2_👜_직장 선택 import choose
 
 # -- import modules end --
 def set_variable():
