bef30467cb6db844e082eea4d40bb9cf2de24163
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-25 10:33:36 +0900

    Update 밸런스_업.py

--- 밸런스_업.py
+++ 밸런스_업.py
@@ -108,7 +108,7 @@ def main():
             <div class="row" style="margin-top: 0%;">
                 <div class="col-md-4"></div>
                 <div class="col-md-5 d-flex justify-content-center">
-                    <img src="data:image/png;base64,{img_to_bytes("./img/balanceup_logo.png")}" style="width:400px; height:350px;">  
+                    <img src="data:image/png;base64,{img_to_bytes("./img/balanceup_logo.png")}" style="width: 200px; height: 200px;">  
                 </div>
                 <div class="col-md-4"></div>
             </div>
