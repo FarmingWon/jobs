5b154d1de3677306f67a210da39874d8d250262c
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-25 10:15:29 +0900

    Update 밸런스_업.py

--- 밸런스_업.py
+++ 밸런스_업.py
@@ -285,7 +285,7 @@ def main():
             <div class="row">
                 <!-- <div style="float: left; margin-left: 25%;"> -->
                 <div class="col">
-                    <h1>개인 맞춤 직무 추천과<br>관련 기업의 인프라 평가까지</h1> 
+                    <h1>개인 맞춤 직무 추천과 관련 기업의 인프라 평가까지</h1> 
                     거대언어모델을 활용한 <strong>개인 커스텀<br>AI 직무 추천</strong>과 밸런스업의 내부 평가<br>
                     모델을 통한 ELEI 차트를 제공합니다.
                     <p><small>ELEI(Enterprise Living Environment Index) :<br>기업 생활 환경 지수 </small></p>
