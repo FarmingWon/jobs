1f5ebf08696da56e722d7ddd4b495e5f1bd74336
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-25 20:14:23 +0900

    Update 밸런스_업.py

--- 밸런스_업.py
+++ 밸런스_업.py
@@ -113,6 +113,7 @@ def main():
     <script
     type="text/javascript"
     src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="./script/jquery-fadethis-master/dist/jquery.fadethis.min.js"></script>
     """
