d33075837c557a7b73caf8753663703c737204a0
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-25 10:32:24 +0900

    Update 밸런스_업.py

--- 밸런스_업.py
+++ 밸런스_업.py
@@ -146,12 +146,15 @@ def main():
     <div class="container" style="margin-bottom: 10%"></div>
     """ + """
     <style type="text/css">
-    @font-face {
-        font-family: 'Pretendard-Regular';
-        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-        font-weight: 400;
-        font-style: normal;
-    }
+        @font-face {
+            font-family: 'Pretendard-Regular';
+            src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+            font-weight: 400;
+            font-style: normal;
+        }
+        body {
+            font-family: 'Pretendard-Regular';
+        }
         .container.wrap{
             overflow: hidden;
             #height: 700px;
