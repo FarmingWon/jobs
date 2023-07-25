1ee7a0f68c0186b1323811ac77a2ef08d0d6a674
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-25 10:21:03 +0900

    Update 밸런스_업.py

--- 밸런스_업.py
+++ 밸런스_업.py
@@ -277,7 +277,7 @@ def main():
     </style>""", unsafe_allow_html=True)
     col1, col2, col3 = st.columns([1,3,1])
     with col2:
-        if st.button("서비스 이용하러 가기"):
+        if st.button("무료로 이용하기"):
             switch_page("이력서를_통한_직업_추천")
 
     html3 = f"""
