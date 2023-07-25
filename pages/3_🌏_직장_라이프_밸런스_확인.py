16986bf4c506a4d5d30ae25e71e13d4552d6019f
wonseok <98411696+FarmingWon@users.noreply.github.com>
2023-07-25 12:03:54 +0900

    Update 3_🌏_직장_라이프_밸런스_확인.py

--- pages/3_🌏_직장_라이프_밸런스_확인.py
+++ pages/3_🌏_직장_라이프_밸런스_확인.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_echarts import st_echarts
 from streamlit_folium import st_folium
 from st_pages import add_page_title
+from streamlit_extras.switch_page_button import switch_page
 
 import folium
 
