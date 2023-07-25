a19cf6f5eeae84257968b66e94a6d95c4538e2b4
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-25 10:14:08 +0900

    Update 밸런스_업.py

--- 밸런스_업.py
+++ 밸런스_업.py
@@ -153,6 +153,12 @@ def main():
     <div class="container" style="margin-bottom: 10%"></div>
     """ + """
     <style type="text/css">
+    @font-face {
+        font-family: 'Pretendard-Regular';
+        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+        font-weight: 400;
+        font-style: normal;
+    }
         .container.wrap{
             overflow: hidden;
             #height: 700px;
@@ -276,14 +282,18 @@ def main():
 
     html3 = f"""
         <div style="margin-top: 30%; background-color: #EDEDED; height: 300px;">
-            <div style="float: left; margin-left: 25%;">
-                <h1>개인 맞춤 직무 추천과<br>관련 기업의 인프라 평가까지</h1> 
-                거대언어모델을 활용한 <strong>개인 커스텀<br>AI 직무 추천</strong>과 밸런스업의 내부 평가<br>
-                모델을 통한 ELEI 차트를 제공합니다.
-                <p>ELEI(Enterprise Living Environment Index) :<br>기업 생활 환경 지수 </p>
-            </div>
-            <div style="float: left;">
-                <img src="data:image/png;base64,{img_to_bytes("img/tmpImg.png")}"  style="height: 300px; width: 300px; margin-left: 10%;">
+            <div class="row">
+                <!-- <div style="float: left; margin-left: 25%;"> -->
+                <div class="col">
+                    <h1>개인 맞춤 직무 추천과<br>관련 기업의 인프라 평가까지</h1> 
+                    거대언어모델을 활용한 <strong>개인 커스텀<br>AI 직무 추천</strong>과 밸런스업의 내부 평가<br>
+                    모델을 통한 ELEI 차트를 제공합니다.
+                    <p>ELEI(Enterprise Living Environment Index) :<br>기업 생활 환경 지수 </p>
+                </div>
+                <!-- <div style="float: left;"> -->
+                <div class="col">
+                    <img src="data:image/png;base64,{img_to_bytes("img/tmpImg.png")}"  style="height: 300px; width: 300px; margin-left: 10%;">
+                </div>
             </div>
         </div>
     """
