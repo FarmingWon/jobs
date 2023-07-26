ff3555f036ab31a5a0cba6c1241e60b8a4fc3cd6
wonseok <98411696+FarmingWon@users.noreply.github.com>
2023-07-26 17:28:56 +0900

    Update 6_스트리밍중인_강의_참여.py

--- pages/6_스트리밍중인_강의_참여.py
+++ pages/6_스트리밍중인_강의_참여.py
@@ -19,24 +19,3 @@ with st.sidebar:
     """
     st.markdown(htmlSide, unsafe_allow_html=True)
     st.sidebar.markdown("---")
-    htmlSide2=f"""
-        <div id="logo">
-            <h5>
-                <span>Powered By  &nbsp; &nbsp; &nbsp;</span>
-                <img src="data:image/png;base64,{img_to_bytes("./img/openai_logo-removebg.png")}" style="width:180px; height:60px;">
-            </h5>
-        </div>
-        <div id="logo">
-            <h5>
-                <span>Powered By  &nbsp; &nbsp; &nbsp;</span>
-                <img src="data:image/png;base64,{img_to_bytes("./img/mongodb logo.png")}" style="width:180px; height:60px;">
-            </h5>
-        </div>
-        <div id="logo">
-            <h5>
-                <span>Powered By  &nbsp; &nbsp; &nbsp;</span>
-                <img src="data:image/png;base64,{img_to_bytes("./img/Neo4j-logo_color.png")}" style="width:180px; height:60px;">
-            </h5>
-        </div>
-        """
-    st.markdown(htmlSide2, unsafe_allow_html=True)
